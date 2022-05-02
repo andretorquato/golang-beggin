2d12f59a61e28eb3f1aaec2f1ba011695c18cc24
André Torquato <andretorquato@alu.ufc.br>
2022-05-02 07:08:16 -0300

    fix import

--- concurrence/generator/main.go
+++ concurrence/generator/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	"../reusable/html"
+	"github.com/andretorquato/golang-beggin/reusable/html"
 )
 
 func main() {
-	t1 := html.Ti("https://www.baidu.com", "https://www.google.com")
-	t2 := title("https://www.gmail.com", "https://www.amazon.com")
+	t1 := html.Title("https://www.baidu.com", "https://www.google.com")
+	t2 := html.Title("https://www.gmail.com", "https://www.amazon.com")
 
 	fmt.Printf("%s | %s\n", <-t1, <-t2)
 	fmt.Printf("%s | %s\n", <-t1, <-t2)
