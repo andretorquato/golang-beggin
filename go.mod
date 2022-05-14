d3f64ac71e1bba7219fdd0d732dcddda7b1fb8d0
André Torquato <andretorquato@alu.ufc.br>
2022-05-14 19:05:27 -0300

    connect with database and create table

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module golang-beggin
 go 1.17
 
 require github.com/andretorquato/golang-beggin v0.0.0-20220502013322-a97c146ac28a
+
+require github.com/go-sql-driver/mysql v1.6.0 // indirect
--- sql/structure/structure.go
+++ sql/structure/structure.go
@@ -1,3 +1,36 @@
 package main
 
-func main() {}
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func exec(db *sql.DB, sql string) sql.Result {
+	result, err := db.Exec(sql)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	exec(db, "CREATE DATABASE IF NOT EXISTS golangcourse")
+	exec(db, "USE golangcourse")
+	exec(db, "DROP TABLE IF EXISTS users")
+	exec(db, `
+		CREATE TABLE users (
+			id INT AUTO_INCREMENT PRIMARY KEY,
+			name VARCHAR(255) NOT NULL,
+			email VARCHAR(255) NOT NULL,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		)
+	`)
+
+}
