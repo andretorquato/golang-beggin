b840cfbe04424117e23f1ced9f2417a4be9dbded
André Torquato <andretorquato@alu.ufc.br>
2022-05-18 23:21:15 -0300

    update row into db with golang

---
+++ sql/update/update.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/golangcourse")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	stmt, _ := db.Prepare("UPDATE users set name = ? where id = ?")
+
+	stmt.Exec("Clark Quiston", 1)
+	stmt.Exec("Jane Maclaren", 2)
+	stmt.Exec("Ronaldo Montblanc", 3)
+}
