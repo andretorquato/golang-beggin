2c8593f4113bb9d10b0bc5e14c65926c482ff78e
André Torquato <andretorquato@alu.ufc.br>
2022-05-19 23:24:54 -0300

    delete in sql

--- sql/update/update.go
+++ sql/update/update.go
@@ -18,4 +18,8 @@ func main() {
 	stmt.Exec("Clark Quiston", 1)
 	stmt.Exec("Jane Maclaren", 2)
 	stmt.Exec("Ronaldo Montblanc", 3)
+
+	stmt2, _ := db.Prepare("DELETE FROM users where id = ?")
+
+	stmt2.Exec(1)
 }
