0d35442c6381f9d34db236a55000cb1476cfade6
André Torquato <andretorquato@alu.ufc.br>
2022-05-23 23:06:10 -0300

    generate dynamic page

--- http/dynamic/dynamic.go
+++ http/dynamic/dynamic.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func now(w http.ResponseWriter, r *http.Request) {
+	s := time.Now().Format("02/01/2006 03:04:05 PM")
+	fmt.Fprintf(w, "<h2>The time is %s </h2>", s)
+}
+
 func main() {
+	http.HandleFunc("/", now)
+	log.Println("executing server...")
+	log.Fatal(http.ListenAndServe(":3000", nil))
 
 }
