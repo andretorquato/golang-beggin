83c8eabe697fcbc461dd900bb5176026a7f03bec
André Torquato <andretorquato@alu.ufc.br>
2022-05-26 23:28:54 -0300

    get user

--- http/simple-server/server.go
+++ http/simple-server/server.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-type user struct {
+type User struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
 }
@@ -26,7 +31,24 @@ func userHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func getUser(w http.ResponseWriter, r *http.Request, id int) {}
+func getUser(w http.ResponseWriter, r *http.Request, id int) {
+	db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/golangcourse")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var u User
+
+	db.QueryRow("SELECT id, name from users where id = ?", id).Scan(&u.ID, &u.Name)
+
+	json, _ := json.Marshal(u)
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, string(json))
+
+}
 
 func getUsers(w http.ResponseWriter, r *http.Request) {}
 
