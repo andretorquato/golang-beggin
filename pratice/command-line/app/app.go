80bfecb3a6bd6d0a48ba6d082a21c6cdc2b095be
André Torquato <andretorquato@alu.ufc.br>
2022-07-13 23:49:13 -0300

    add command to get ips from a url

--- pratice/command-line/app/app.go
+++ pratice/command-line/app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli/v2"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli/v2"
+)
 
 // Generate a new app.
 func Generate() *cli.App {
@@ -8,5 +14,31 @@ func Generate() *cli.App {
 	app.Name = "Command Line Interface"
 	app.Usage = "A CLI for the Go programming language."
 
+	app.Commands = []*cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Get the IP address from the internet",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:  "host",
+					Value: "google.com.br",
+				},
+			},
+			Action: getIps,
+		},
+	}
 	return app
+}
+
+func getIps(c *cli.Context) error {
+	host := c.String("host")
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+
+	return nil
 }
