de2c3acf46f9b2600f3e1284c62ba3ad0979db05
André Torquato <andretorquato@alu.ufc.br>
2022-07-20 00:26:51 -0300

    configure route

--- pratice/devbook/api/src/router/router.go
+++ pratice/devbook/api/src/router/router.go
@@ -1,7 +1,12 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"api/src/router/routes"
+
+	"github.com/gorilla/mux"
+)
 
 func Start() *mux.Router {
-	return mux.NewRouter()
+	r := mux.NewRouter()
+	return routes.Configure(r)
 }
--- pratice/devbook/api/src/router/routes/route.go
+++ pratice/devbook/api/src/router/routes/route.go
@@ -1,10 +1,23 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type Route struct {
 	URI      string
 	Method   string
 	Handler  func(w http.ResponseWriter, r *http.Request)
 	NeedAuth bool
+}
+
+func Configure(r *mux.Router) *mux.Router {
+	routes := userRoutes
+	for _, route := range routes {
+		r.HandleFunc(route.URI, route.Handler).Methods(route.Method)
+	}
+
+	return r
 }
