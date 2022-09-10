865197382c9f74d04704ddf671e0ff3ad1618869
André Torquato <andretorquato@alu.ufc.br>
2022-09-09 23:28:06 -0300

    rendering templates html

---
+++ pratice/devbook/api/web/utils/templates.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var templates *template.Template
+
+func LoadTemplate() {
+	templates = template.Must(template.ParseGlob("views/*.html"))
+}
+
+func ExecuteTemplate(w http.ResponseWriter, template string, data interface{}) {
+	templates.ExecuteTemplate(w, template, data)
+}
