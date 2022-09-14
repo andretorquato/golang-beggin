285ee6d7536d9afe3663f41e02086e08faa33936
André Torquato <andretorquato@alu.ufc.br>
2022-09-13 23:39:48 -0300

    fix

--- pratice/devbook/api/web/assets/js/register.js
+++ pratice/devbook/api/web/assets/js/register.js
@@ -2,17 +2,20 @@ $("#register-form").on(createUser);
 
 function createUser(event){
 	event.preventDefault();
-
+	console.log("registe")
 	if($("#password").val() != $("#password-confirm").val()){
 		alert("fill the same password");
 		return
 	}
 
 	$.ajax({
-		url: "users",
+		url: "/users",
 		method: "POST",
 		data: {
-			name: $("#name").val()
+			name: $("#name").val(),
+			email: $("#email").val(),
+			nick: $("#nick").val(),
+			password: $("#password").val()
 		}
 	});
 }
