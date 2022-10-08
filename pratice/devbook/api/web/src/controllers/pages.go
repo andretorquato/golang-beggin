668d4b603cc7b15d17ae44148e63a4c9b26a9347
André Torquato <ats.torquato1@gmail.com>
2022-10-07 22:29:14 -0300

    render posts

--- pratice/devbook/api/web/src/controllers/pages.go
+++ pratice/devbook/api/web/src/controllers/pages.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"web/src/config"
+	"web/src/models"
 	"web/src/requests"
+	"web/src/responses"
 	"web/src/utils"
 )
 
@@ -18,8 +21,24 @@ func LoadRegisterScreen(w http.ResponseWriter, r *http.Request) {
 
 func LoadHomeScreen(w http.ResponseWriter, r *http.Request) {
 	url := fmt.Sprintf("%s/posts", config.APIURL)
-	response, erro := requests.MakeRequestWithAuthentication(r, http.MethodGet, url, nil)
-	fmt.Println(response.StatusCode, erro)
+	res, erro := requests.MakeRequestWithAuthentication(r, http.MethodGet, url, nil)
+	if erro != nil {
+		responses.JSON(w, http.StatusInternalServerError, responses.ErroAPI{Erro: erro.Error()})
+		return
+	}
+	defer res.Body.Close()
 
-	utils.ExecuteTemplate(w, "home.html", nil)
+	if res.StatusCode >= 400 {
+		responses.JSON(w, http.StatusUnprocessableEntity, responses.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	var posts []models.Post
+
+	if erro = json.NewDecoder(res.Body).Decode(&posts); erro != nil {
+		responses.JSON(w, http.StatusUnprocessableEntity, responses.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	utils.ExecuteTemplate(w, "home.html", posts)
 }
